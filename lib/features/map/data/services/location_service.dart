51693f6c83d0d5b30140b0303e8f87ffe2acfa29
marcdevelopez <marcdevelopez@gmail.com>
2025-05-29 08:30:30 +0200

    Add LocationService for getting current position using Geolocator

---
+++ lib/features/map/data/services/location_service.dart
@@ -0,0 +1,25 @@
+import 'package:geolocator/geolocator.dart';
+
+class LocationService {
+  static Future<Position> getCurrentPosition() async {
+    bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      throw Exception('Location service are disabled');
+    }
+
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        throw Exception('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      throw Exception('Lopcation permissions are permanently denied.');
+    }
+
+    final settings = LocationSettings(accuracy: LocationAccuracy.high);
+    return await Geolocator.getCurrentPosition(locationSettings: settings);
+  }
+}
